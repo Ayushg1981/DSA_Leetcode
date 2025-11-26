dd4da33ebea15486942af8b486d0ba52e644b18e
Ayushg1981 <158587980+Ayushg1981@users.noreply.github.com>
2025-11-26 11:19:54 +0530

    Time: 55 ms (29.85%) | Memory: 46.7 MB (21.18%) - LeetSync

---
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void f(set<vector<int>>& st,vector<int>& nums,vector<int>& a,int i){
+        if(i==nums.size()){
+            if(a.size()>1) st.insert(a);
+            return;
+        }
+        int n=a.size();
+        f(st,nums,a,i+1);
+        if(n==0 || a[n-1]<=nums[i]){
+            a.push_back(nums[i]);
+            f(st,nums,a,i+1);
+            a.pop_back();
+        }
+    }
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        set<vector<int>> st;
+        vector<vector<int>> ans;
+        vector<int> a;
+        f(st,nums,a,0);
+        for(auto x:st){
+            ans.push_back(x);
+        }
+        return ans;
+    }
+};
