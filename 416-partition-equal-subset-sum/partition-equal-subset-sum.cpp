175cbf736493449bd126594a6ddb97172457ce3b
Ayushg1981 <158587980+Ayushg1981@users.noreply.github.com>
2025-12-09 10:01:17 +0530

    Time: 175 ms (20.31%) | Memory: 17.5 MB (53.28%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -15,6 +15,22 @@ public:
             c+=nums[i];
         }
         if(c%2==1) return false;
-        return f(nums,0,c/2);
+        // return f(nums,0,c/2);
+
+        vector<vector<bool>> dp(nums.size(),vector<bool>(c/2+1,false));
+        for(int i=0;i<nums.size();i++){
+            dp[i][0]==true;
+        }
+        if(nums[0]>c/2) return false;
+        dp[0][nums[0]]=true;
+        for(int i=1;i<nums.size();i++){
+            for(int j=0;j<=c/2;j++){
+                dp[i][j]=dp[i-1][j];
+                if(j-nums[i]>=0){
+                    dp[i][j]=dp[i][j]||dp[i-1][j-nums[i]];
+                }
+            }
+        }
+        return dp[nums.size()-1][c/2];
     }
 };
