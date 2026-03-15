e41437d9018b34819e3ba872ed852c0368f4133a
Ayushg1981 <158587980+Ayushg1981@users.noreply.github.com>
2026-03-15 10:00:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (56.11%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+typedef long long ll;
+    long long countCommas(long long n) {
+        ll ans = 0;
+
+        for(int i = 4; i <= 18; i++) {
+            ll curr = (i - 1) / 3;
+
+            ll l = 1;
+            for(int j = 1; j < i; j++) {
+                l *= 10;
+            }
+
+            ll r = l * 10 - 1;
+
+            if(l > n) break;
+
+            ll cnt = min(r, n) - l + 1;
+            ans += cnt * curr;
+        }
+
+        return ans;
+    }
+};
