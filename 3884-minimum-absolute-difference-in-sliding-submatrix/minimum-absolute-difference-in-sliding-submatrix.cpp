a14d645a7a09fa141f24f7c5a092678679c6d99e
Ayushg1981 <158587980+Ayushg1981@users.noreply.github.com>
2026-03-20 09:49:18 +0530

    Time: 18 ms (58.52%) | Memory: 32.1 MB (68.15%) - LeetSync

--- 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.cpp
+++ 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.cpp
@@ -7,27 +7,20 @@ public:
         
         for(int i=0;i<=m-k;i++){
             for(int j=0;j<=n-k;j++){
-                set<int>s;
+                vector<int> v;
                 for(int r=i;r<i+k;r++){
                     for(int c=j;c<j+k;c++){
-                        s.insert(grid[r][c]);
+                        v.push_back(grid[r][c]);
                     }
                 }
-                if(s.size()<=1) ans[i][j]=0;
-                else{
-                    vector<int> v(s.begin(), s.end());        
-                    sort(v.begin(),v.end());
-                    int q=INT_MAX;
-                    for(int u=1;u<v.size();u++){
-                        if(v[u]!=v[u-1]){
-                            q=min(q,abs(v[u]-v[u-1]));
-                        }
-                        else{
-                            q=0;    
-                        }
+                sort(v.begin(),v.end());
+                int q=INT_MAX;
+                for(int u=1;u<v.size();u++){
+                    if(v[u]!=v[u-1]){
+                        q=min(q,abs(v[u]-v[u-1]));
                     }
-                    ans[i][j]=q==INT_MAX?0:q;
                 }
+                ans[i][j]=q==INT_MAX?0:q;
             }
         }
         return ans;
