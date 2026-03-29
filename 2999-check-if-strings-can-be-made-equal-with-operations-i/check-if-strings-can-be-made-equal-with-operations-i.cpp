d2f33ac7090fc993863509473dbe03c3839c2abe
Ayushg1981 <158587980+Ayushg1981@users.noreply.github.com>
2026-03-29 10:13:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (67.10%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-        if(s1[0]!=s2[0]&&s1[0]!=s2[2])return false;
-        if(s1[1]!=s2[1]&&s1[1]!=s2[3])return false;
-        if(s2[0]!=s1[0]&&s2[0]!=s1[2])return false;
-        if(s2[1]!=s1[1]&&s2[1]!=s1[3])return false;
-        if(s1[3]!=s2[3]&&s1[3]!=s2[1])return false;
-        if(s1[2]!=s2[2]&&s1[2]!=s2[0])return false;
-        if(s2[3]!=s1[3]&&s2[3]!=s1[1])return false;
-        if(s2[2]!=s1[2]&&s2[2]!=s1[0])return false;
-        return true;
+        for(int i = 0; i < 2; i++){
+            if(s1[i] != s2[i]){
+                swap(s1[i], s1[i+2]);
+            }
+        }
+        return s1 == s2;
     }
 };
