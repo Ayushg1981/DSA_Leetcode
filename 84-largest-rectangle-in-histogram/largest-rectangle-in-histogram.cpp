be8eca9c3fe359aa3e6bee9874c00da24ddaf105
Ayushg1981 <158587980+Ayushg1981@users.noreply.github.com>
2026-04-16 10:43:27 +0530

    Time: 27 ms (38.55%) | Memory: 85.7 MB (41.34%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -4,10 +4,7 @@ public:
         stack<int> s1,s2;
         int n=h.size();
         vector<int> a(n),b(n);
-        a[0]=-1;
-        b[n-1]=n;
-        s1.push(0);
-        for(int i=1;i<n;i++){
+        for(int i=0;i<n;i++){
             while(s1.size()>0 && h[s1.top()]>=h[i]) s1.pop();
             if(s1.empty()) a[i]=-1;
             else{
@@ -15,8 +12,7 @@ public:
             }
             s1.push(i);
         }
-        s2.push(n-1);
-        for(int i=n-2;i>=0;i--){
+        for(int i=n-1;i>=0;i--){
             while(s2.size()>0 && h[s2.top()]>=h[i]) s2.pop();
             if(s2.empty()) b[i]=n;
             else{
