714940ed47a302a355a3b44edda83acc850a43d6
Ayushg1981 <158587980+Ayushg1981@users.noreply.github.com>
2026-04-17 19:29:22 +0530

    Time: 242 ms (6.04%) | Memory: 49.4 MB (7.04%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -2,12 +2,6 @@ class Solution {
 public:
     int f(int i , int j , vector<int> & arr ,  vector<vector<int>> &dp) {
         if(i>j) return 0;
-        if(i==j){ 
-            int x=arr[i];
-            if(j+1<arr.size()) x*=arr[j+1];
-            if(i-1>=0) x*=arr[i-1];
-            return x;
-        }
         if(dp[i][j] != -1) return dp[i][j] ;
         int ans = INT_MIN ;
         for(int k = i ; k<=j ; k++) {
