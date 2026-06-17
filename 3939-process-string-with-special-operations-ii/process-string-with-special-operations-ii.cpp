20d90596e6ac0e0b738ccefe937a0fd33c1d41fa
Ayushg1981 <158587980+Ayushg1981@users.noreply.github.com>
2026-06-17 10:44:04 +0530

    Time: 27 ms (62.07%) | Memory: 24.1 MB (51.72%) - LeetSync

---
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    char processStr(string s, long long k) {
+        long long len=0;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]=='*') len=max(len-1,0LL);
+            else if(s[i]=='#') len*=2;
+            else if(s[i]!='%') len++;
+        }
+        if(k>=len) return '.';
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='*') len++;
+            else if(s[i]=='#'){
+                if(k>=len/2) k-=len/2;
+                len/=2;
+            }
+            else if(s[i]=='%') k=len-1-k;
+            else if(len==(k+1)) return s[i];
+            else len--;
+        }
+        return '.';
+    }
+};
