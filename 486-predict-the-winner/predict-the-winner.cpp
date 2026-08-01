fdce48fc1e8f60162e38f033e2acbef3a86f0966
Ayushg1981 <158587980+Ayushg1981@users.noreply.github.com>
2026-08-01 12:04:34 +0530

    Time: 91 ms (22.18%) | Memory: 9.6 MB (85.12%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
-    int dp[25][25];
     int f(int i,int j,vector<int>& nums){
         if(j==i) return nums[i];
-        if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=max(nums[i]-f(i+1,j,nums),nums[j]-f(i,j-1,nums));
+        return max(nums[i]-f(i+1,j,nums),nums[j]-f(i,j-1,nums));
     }
     bool predictTheWinner(vector<int>& nums) {
-        memset(dp,-1,sizeof dp);
         return (f(0,nums.size()-1,nums)>=0);
     }
 };
